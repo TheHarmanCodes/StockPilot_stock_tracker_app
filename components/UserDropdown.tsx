4aa8f3eccaa417480d7b20a3f44fd87c22c25b14
TheHarmanCodes <harmansingh444444@gmail.com>
2026-04-03 17:11:06 +0530

    Implement Search Stocks

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -15,7 +15,13 @@ import { LogOut } from "lucide-react";
 import NavItems from "./NavItems";
 import { signOut } from "@/lib/actions/auth.actions";
 
-const UserDropdown = ({ user }: { user: User }) => {
+const UserDropdown = ({
+  user,
+  initialStocks,
+}: {
+  user: User;
+  initialStocks: StockWithWatchlistStatus;
+}) => {
   const router = useRouter();
 
   const handleSignOut = async () => {
